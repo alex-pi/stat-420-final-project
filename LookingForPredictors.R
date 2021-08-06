d77e00107fed2e64fec5f05cbe21afdef777ef9b
alejanpi <alejandro.pimentel_rios@nokia.com>
2021-08-06 14:26:43 -0600

    Just experimenting with predictors transformations

--- LookingForPredictors.R
+++ LookingForPredictors.R
@@ -30,8 +30,16 @@ loans_nona <- subset(loans_nona,
                        loan_status,
                        initial_listing_status,
                        disbursement_method,
-                       #grade,
+                       grade,
                        sub_grade
+                     ))
+
+loans_nona <- subset(loans_nona, 
+                     select = -c(
+                       homeownership,
+                       verified_income,
+                       application_type,
+                       term
                      ))
 
 
@@ -41,20 +49,22 @@ loans_nona$homeownership <- as.factor(loans_nona$homeownership)
 loans_nona$verified_income <- as.factor(loans_nona$verified_income)
 loans_nona$application_type <- as.factor(loans_nona$application_type)
 loans_nona$term <- as.factor(loans_nona$term)
-loans_nona$grade <- as.factor(loans_nona$grade)
+#loans_nona$grade <- as.factor(loans_nona$grade)
 #loans_nona$sub_grade <- as.factor(loans_nona$sub_grade)
 
 
 sapply(loans_nona, function(x) sum(is.na(x)))
 
 idxs <- 1:nrow(loans_nona)
-ran_idx = sample(idxs, 7000)
+ran_idx = sample(idxs, 2000)
 
 loans_sampled <- loans_nona[ran_idx, ]
 
 small <- lm(log(interest_rate) ~ grade, data = loans_sampled)
 
 additive <- lm(log(interest_rate) ~ ., data = loans_sampled)
+
+additive2 <- lm(interest_rate ~ ., data = loans_sampled)
 
 slct_from_add <- step(additive, k = log(nrow(loans_sampled)), trace = 0)
 
@@ -66,12 +76,25 @@ slct_from_add_nograde <- step(additive_nograde, k = log(nrow(loans_sampled)), tr
 
 #slct <- step(model, k = log(nrow(loans_sampled)), trace = 0)
 
-summary(slct_from_add_nograde)$r.squared
-summary(slct_from_add_nograde)$adj.r.squared
+summary(additive2)$r.squared
+summary(additive2)$adj.r.squared
 
 vif(slct_from_add_nograde)
 
-diagnostic_graph(slct_from_add_nograde)
+## This 2 functions are in the report
+diagnostic_graph(additive2)
+diagnostics(slct_from_add)
+
+plot(interest_rate ~ debt_to_income, 
+     data = loans_sampled, col = "grey", pch = 20,
+     main = "Data from Model 1")
+
+#pairs(loans_sampled)
+
+
+
+
+
 
 
 
