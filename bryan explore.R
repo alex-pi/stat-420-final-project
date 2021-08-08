c47d246708df806db9508ed4931177fbfdeba362
CapnDanger <bpholcomb94@gmail.com>
2021-08-07 23:44:11 -0500

    collinearity added

--- bryan explore.R
+++ bryan explore.R
@@ -1,9 +1,12 @@
 loans_nona = subset(loans_nona, select = -c(grade, sub_grade))
 
+idxs <- 1:nrow(loans_nona)
 ran_idx = sample(idxs, 3000)
 loans_sampled = loans_nona[ran_idx,]
 
 loans_num = subset(loans_sampled, select = -c(homeownership, verified_income, loan_purpose, application_type, term))
+
+loans_sampled = subset(loans_sampled, select = -c(current_accounts_delinq, num_accounts_30d_past_due))
 
 round(cor(loans_num), 3)
 
@@ -27,11 +30,7 @@ vif(add_model)[vif(add_model) > 5]
 
 loans_sampled = subset(loans_sampled, select = -c(num_open_cc_accounts,
                                                   num_cc_carrying_balance,
-                                                  num_historical_failed_to_pay,
-                                                  paid_total,
-                                                  paid_principal,
-                                                  paid_interest,
-                                                  paid_late_fees))
+                                                  num_historical_failed_to_pay))
 add_model = lm(interest_rate ~ ., data = loans_sampled)
 round(vif(add_model)[vif(add_model) > 5],2)
 length(coef(add_model))
